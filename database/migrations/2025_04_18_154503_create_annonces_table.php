baf8127a60b79534cd27fc47754df51bac079155
Luxanna23 <yasmineadh@gmail.com>
2025-06-21 10:32:10 +0200

    modif migration

--- database/migrations/2025_04_18_154503_create_annonces_table.php
+++ database/migrations/2025_04_18_154503_create_annonces_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('annonces', function (Blueprint $table) {
             $table->id('id');
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->string('address');
             $table->string('postal_code');
             $table->string('city');
